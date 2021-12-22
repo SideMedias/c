4a218f7dc736f92f940b9ba7f2521e1af1655d4d
SideMedias <96510602+SideMedias@users.noreply.github.com>
2021-12-22 13:43:36 +0900

    Create triforce.c

---
+++ triforce.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+	int i,j,num=10;
+//	while(1){
+	for(i=0;i<num;i++){
+		for(j=num;j>i+1;j--){
+			printf(" ");
+		}
+		for(j=0;j<i;j++){
+			printf(" ");
+		}
+		for(j=num;j>i;j--){
+			printf(" ");
+		}
+		for(j=0;j<i*2+1;j++){
+			printf("3");		
+		}
+		printf("\n");
+	}
+	for(i=0;i<num;i++){
+		for(j=num;j>i+1;j--){
+			printf(" ");
+		}
+		for(j=0;j<i*2+1;j++){
+			printf("3");		
+		}
+		for(j=num;j>i+1;j--){
+			printf(" ");
+		}
+		for(j=num;j>i;j--){
+			printf(" ");
+		}
+		for(j=0;j<i*2+1;j++){
+			printf("3");		
+		}
+		printf("\n");
+	}
+//	}
+	return 0;
+}
